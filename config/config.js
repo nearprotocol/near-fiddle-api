ad3958a64ed272653cfa352f6d3e648da7e55b1e
Vladimir Grichina <vgrichina@gmail.com>
2019-07-02 15:42:34 -0700

    Use config.js for sequelize so that env variables can be used

---
+++ config/config.js
@@ -0,0 +1,30 @@
+module.exports = {
+    development: {
+        username: 'fiddle',
+        password: 'fiddle',
+        database: 'fiddle',
+        host: '127.0.0.1',
+        dialect: 'postgres',
+    },
+    test: {
+        username: 'fiddle',
+        password: 'fiddle',
+        database: 'fiddle_test',
+        host: '127.0.0.1',
+        dialect: 'postgres',
+    },
+    ci: {
+        username: 'fiddle',
+        password: 'fiddle',
+        database: 'fiddle_test',
+        host: 'postgres',
+        dialect: 'postgres',
+    },
+    production: {
+        username: process.env.FIDDLE_DB_USERNAME || 'fiddle',
+        password: process.env.FIDDLE_DB_PASSWORD || 'fiddle',
+        database: process.env.FIDDLE_DB_NAME || 'fiddle',
+        host: process.env.FIDDLE_DB_HOST || '127.0.0.1',
+        dialect: 'postgres',
+    },
+}
